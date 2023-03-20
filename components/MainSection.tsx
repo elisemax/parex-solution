ac7bbc998b0cd5c8d2eec2e13010ac7124b16631
Max <max11cz@seznam.cz>
2023-03-20 21:51:59 +0100

    add

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -9,14 +9,14 @@ type Props = {
 export default function MainSection(props: Props) {
     const { title, description, button } = props
   return (
-        <div className="w-full h-full bg-no-repeat bg-cover bg-[url('../img/bg.jpeg')] bg-black/70 opacity-60 bg-blend-darken">
+        <div className="w-full h-full bg-no-repeat bg-cover bg-[url('../img/bg.jpeg')] bg-black/70 bg-blend-darken">
             <div className="flex flex-col justify-center items-center h-screen">
             <h2 className="text-6xl font-bold text-white text-center">{title}</h2>
             <p className="block text-2xl text-white text-center m-3">{description}</p>
             <ScrollLink href="#contact">
               <button
                 type="button"
-                className="rounded bg-red-600 py-2.5 px-14 text-base font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                className="rounded bg-red-600 py-2.5 px-14 text-base font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 opacity-100 transition duration-500 ease-in-out"
               >
                 <div className='whitespace-nowrap inline'>{button}</div>
           </button>
