5ac0e15d8aae30fba16c58615b828a937909e1c4
Max <max11cz@seznam.cz>
2023-03-20 22:16:02 +0100

    add

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-        <div className="min-h-[15rem] flex flex-col bg-white rounded-xl ">
-            <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
+        <div className="min-h-[10rem] flex flex-col bg-white rounded-xl ">
+            <div className="flex flex-auto flex-col justify-center items-center p-4 px-24 md:p-5">
                 <div className="flex justify-center">
                 <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-red-600 rounded-full" role="status" aria-label="loading">
                     <span className="sr-only">Loading...</span>
