d98dabea539d5ab4396b83326307e8933a9eaed3
Max <max11cz@seznam.cz>
2023-03-21 19:28:23 +0100

    add

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer(props: Props) {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2023 Parex Partners, Inc. All rights reserved.
           </p>
         </div>
       </div>
